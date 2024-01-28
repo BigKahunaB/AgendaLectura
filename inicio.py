206d39b46a51b7e1d533e4188abd0ab1b9000da3
BigKahunaB <140110945+BigKahunaB@users.noreply.github.com>
2024-01-27 21:45:34 -0600

    Add files via upload

---
+++ inicio.py
@@ -0,0 +1,13 @@
+print("Bienvenido a tu agenda de lectura")
+
+titulo = input("Introduce el titulo de tu libro?: ")
+print(titulo)
+opinion = input("Cual es tu opinion sobre el titulo?: ")
+print(opinion)
+loMejor = input("Que te gusto mas?: ")
+print(loMejor)
+loPeor = input("Que te gusto mas?: ")
+print(loPeor)
+cita = input("Cual es tu cita favorita?: ")
+print(cita)
+
